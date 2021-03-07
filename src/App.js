c0efe2ef51c42a7cf20394dad0a065d9fe9e8076
ghost <fayyad_r22@yahoo.com>
2021-03-07 14:27:23 +0100

    add login component and style the link and render the lonig component in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React from "react";
 import "./App.css";
+import Login from "./Login";
 
 function App() {
   return (
     <div className="app">
-      <h1>I build spotyif clone</h1>
+      <Login />
     </div>
   );
 }
