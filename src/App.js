eeeb4afc439d1ecf61a9dd8b785e84849fd465e1
ghost <fayyad_r22@yahoo.com>
2021-03-07 14:04:17 +0100

    clean the style

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div className="app">
+      <h1>I build spotyif clone</h1>
     </div>
   );
 }
