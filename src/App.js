a2b2cb71a352c09c8aeec3afe42f7c44e1698e26
ghost <fayyad_r22@yahoo.com>
2021-03-08 10:02:38 +0100

    get the token from url and clear the url and stor inside state and add if condition to check if has token login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
+  // short time memore stor to save value and not lost if refrech
+  const [token, setToken] = useState(null);
+
+  // run code based on a given condition
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+
+    // clear the endurl
+    window.location.hash = "";
+    const _token = hash.access_token;
+    // token inside memore
+    if (_token) {
+      setToken(_token);
+    }
+    console.log("I have The Token", token);
+  }, []);
   return (
-    <div className="app">
-      <Login />
-    </div>
+    <div className="app">{token ? <h1>I am logged in</h1> : <Login />}</div>
   );
 }
 
