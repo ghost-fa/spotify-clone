0f12e84f35ab1acad6981e17b9d16ad8e5c20f18
ghost <fayyad_r22@yahoo.com>
2021-03-08 09:23:28 +0100

    add spotify.js and add the autendpoint and pass all scops url argement

---
+++ src/spotify.js
@@ -0,0 +1,13 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000/";
+
+const clientId = "b4fa7027b8dd4a038984f96e62d14be9";
+
+const scopes = [
+  `user-read-currently-playing user-read-recently-played user-read-playback-state user-top-read user-modify-playback-state`,
+];
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%/20"
+)}&response_type=token&show_dialog=true`;
