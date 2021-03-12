b68721a2bb3f808f282a00f75d37c2f1a0a50aa5
ghost <fayyad_r22@yahoo.com>
2021-03-12 13:22:29 +0100

    add songRow component to render the song on the body

--- src/Body.js
+++ src/Body.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./Body.css";
 import Header from "./Header";
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import { useDataLayerValue } from "./DataLayer";
+import SongRow from "./SongRow";
 
 function Body(spotify) {
   const [{ discover_Weekly }, dispatch] = useDataLayerValue();
@@ -15,6 +19,16 @@ function Body(spotify) {
           <h2>Discover Weekly</h2>
           <p>{discover_Weekly?.description}.</p>
         </div>
+      </div>
+      <div className="body__songs">
+        <div className="body__icons">
+          <PlayCircleFilledIcon className="body__shuffle" />
+          <FavoriteIcon fontSize="large" />
+          <MoreHorizIcon />
+        </div>
+        {discover_Weekly?.tracks.items.map((item) => {
+          <SongRow track={item.track} />;
+        })}
       </div>
     </div>
   );
